a8fd5cf79be326be8c3a6a5ee73a1d2166d180e8
Binay <binayaraj.thapa@gmail.com>
2022-03-26 15:48:18 +0545

    Adding files

---
+++ binaya/mainwindow.cpp
@@ -0,0 +1,87 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+#include<QDebug>
+#include<QTimer>
+#include<QDateTime>
+#include<QPixmap>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QPixmap pix("C:/Users/binay/OneDrive/Desktop/logo/logo");
+    ui->logo_billx->setPixmap(pix.scaled(50,50,Qt::KeepAspectRatio));
+
+    QPixmap pixmap;
+    pixmap.load(":/background.jpg");
+
+    //scaling the image, optional. See the documentation for more options
+    pixmap = pixmap.scaled(QSize(1920, 1080), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+    QPalette palette;
+    palette.setBrush(QPalette::Window, pixmap);
+    this->setPalette(palette);
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()),this,SLOT(showTime()));
+    timer->start();
+
+
+}
+void MainWindow:: showTime()
+{
+    QTime time=QTime::currentTime();
+    QString time_text=time.toString("hh : mm : ss");
+    if ((time.second()%2)==0)
+    {
+        time_text[3]= ' ';
+        time_text[8]= ' ';
+    }
+    ui->DigitalClock->setText(time_text);
+
+}
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox::StandardButton reply = QMessageBox ::question(this,"Hey!!","Do you want to apply scheme ??",QMessageBox::Yes | QMessageBox::No);
+
+    if(reply == QMessageBox :: Yes){
+        QApplication::quit();
+    } else {
+        qDebug() << "No is clicked";
+
+    }
+
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_5_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+
+}
+
