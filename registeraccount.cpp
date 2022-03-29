cb44cc1b7b3fa36e4b94e1beb50db893bf2b7c46
hotrice37 <96691859+hotrice37@users.noreply.github.com>
2022-03-29 21:38:57 +0545

    Register account

--- registeraccount.cpp
+++ registeraccount.cpp
@@ -1,11 +1,17 @@
 #include "registeraccount.h"
 #include "ui_registeraccount.h"
+#include "mainwindow.h"
 
 RegisterAccount::RegisterAccount(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::RegisterAccount)
 {
     ui->setupUi(this);
+    MainWindow conn;
+    if (!conn.connectionStart())
+        ui -> label_registeraccount_connectionstatus -> setText("Database Fail");
+    else
+        ui ->label_registeraccount_connectionstatus-> setText("Database Connected");
 }
 
 RegisterAccount::~RegisterAccount()
