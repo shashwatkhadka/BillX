83ff074ce07aac35dc41358b8e4e67c2d87e6ee2
siddhartha pradhan <siddhartha.pradhan.08@gmail.com>
2022-04-03 01:11:11 +0545

    searh feature added

--- inventory.cpp
+++ inventory.cpp
@@ -32,3 +32,41 @@ void Inventory::on_pushButton_2_clicked()
     qDebug() <<(modal->rowCount());
 }
 
+
+void Inventory::on_searchbutton_clicked()
+{
+    MainWindow conn;
+    QString name;
+    name=ui->search->text();
+    QSqlQueryModel * modal=new QSqlQueryModel();
+
+    conn.connectionStart();
+    QSqlQuery* qry=new QSqlQuery(conn.mydb);
+
+    qry->prepare("select * from inventory where item='"+name+"'");
+
+    qry -> exec();
+    modal->setQuery(*qry);
+    ui->tableView->setModel(modal);
+
+    conn.connectionClose();
+    qDebug() <<(modal->rowCount());
+
+}
+/*MainWindow conn;
+QString eid;
+eid=ui->searchbutton->text();
+
+if(!conn.connectionStart()){
+
+    qDebug()<<"failed to open database";
+    return ;
+}
+conn.connectionStart();
+QSqlQuery qry;
+qry.prepare("select * from inventory where productid='"+eid+"'");
+*/
+
+
+
+
--- inventory.h
+++ inventory.h
@@ -18,6 +18,8 @@ public:
 private slots:
     void on_pushButton_2_clicked();
 
+    void on_searchbutton_clicked();
+
 private:
     Ui::Inventory *ui;
 };
