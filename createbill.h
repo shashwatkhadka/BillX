1650008f839e06eccf01950d0ad0965c394a7492
hotrice37 <96691859+hotrice37@users.noreply.github.com>
2022-04-10 10:48:25 +0545

    Bill added

--- createbill.h
+++ createbill.h
@@ -28,10 +28,11 @@ private slots:
 
     void on_pushButton_clicked();
 
+    void on_pushButton_save_clicked();
 
-    void on_pushButton_2_clicked();
+    void on_pushButton_print_clicked();
 
-    void on_pushButton_5_clicked();
+    void on_tableView_activated(const QModelIndex &index);
 
 private:
     Ui::CreateBill *ui;
